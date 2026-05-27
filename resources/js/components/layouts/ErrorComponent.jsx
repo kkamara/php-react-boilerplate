c14993f3425352929b22d27ca0ed188c55852914
kkamara <kamaracomputers@gmail.com>
2026-05-27 18:00:38 +0100

    Rename ErrorComponent default exported function name

--- resources/js/components/layouts/ErrorComponent.jsx
+++ resources/js/components/layouts/ErrorComponent.jsx
@@ -1,6 +1,6 @@
 import React from "react"
 
-export default function Error({ error, }) {
+export default function ErrorComponent({ error, }) {
   if (!error || "Token not set." === error) {
     return null
   }
